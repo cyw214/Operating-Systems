a9c4e3cffc236ff903aa1695c44b4d30dd6bdef0
Charles Wallace <cwallace1014@gmail.com>
2017-07-09 03:34:16 -0400

    Delete libpart4.c

--- User Level Root Kit/libpart4.c
+++
@@ -1,6 +0,0 @@
-/*
- * We aren't providing much code here.  You'll need to implement your own
- * dlsym(), as well as any constructors or destructors for your library.
- */
-
-/* TODO: Your code goes here */
